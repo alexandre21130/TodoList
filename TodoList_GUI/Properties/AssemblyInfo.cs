3c0ac59767e2faca973592be0b7976452cc6a079
Alexandre <alexandre21130@free.fr>
2019-03-03 15:03:46 +0100

    Version 19.03.03

--- TodoList_GUI/Properties/AssemblyInfo.cs
+++ TodoList_GUI/Properties/AssemblyInfo.cs
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // Vous pouvez spécifier toutes les valeurs ou indiquer les numéros de build et de révision par défaut 
 // en utilisant '*', comme indiqué ci-dessous :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.19.02.19")]
-[assembly: AssemblyFileVersion("1.19.02.19")]
+[assembly: AssemblyVersion("1.19.03.03")]
+[assembly: AssemblyFileVersion("1.19.03.03")]
